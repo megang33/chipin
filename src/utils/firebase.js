85804fb17cb148c397115e6363b89760baaf7c2a
Megan Go <Meganggo11@gmail.com>
2022-05-01 21:10:32 -0700

    google sign in with button
    - user can create an account and saves to firebase authentication
    - currently connected to a button "sign in with google"
    - redirects user to a google sign in page

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,3 +18,14 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+export const auth = getAuth(app);
+export const signInWithGoogle = () => {
+    signInWithRedirect(auth, provider)
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log("error: " + error);
+        });
+}
