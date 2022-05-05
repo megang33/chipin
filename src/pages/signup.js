8992106b89a488865949b7d8a2ab59199141fad9
Megan Go <Meganggo11@gmail.com>
2022-05-05 01:46:02 -0700

    form input fields and icons
    - added basic input fields for sign up page
    - added icons for each field (not inside the fields yet)

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faLocationDot, faPhone, faAddressCard } from '@fortawesome/free-solid-svg-icons'
+
 const SignUp = () => {
   return (
     <div>
-      <h3>Let's get you started.</h3>
+      <h2 style={{ textAlign: "center" }}>Let's get you started.</h2>
+      <form>
+        <span>
+          <FontAwesomeIcon icon={faUser} />
+          <input type="text" placeholder="name" />
+        </span>
+        <FontAwesomeIcon icon={faAddressCard} />
+        <input type="text" placeholder="affiliation" />
+        <FontAwesomeIcon icon={faPhone} />
+        <input type="text" placeholder="phone number" />
+        <FontAwesomeIcon icon={faLocationDot} />
+        <input type="text" placeholder="zip code" />
+      </form>
     </div>
   );
 }
