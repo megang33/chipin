9bdef5d3cbc89812f78a5d14a68d9b2b40606544
yuchenlungucla <yuchenlung@g.ucla.edu>
2022-05-12 00:23:07 -0700

    Navbar initial styling and format

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,32 +1,31 @@
 import React from 'react';
-import '../index.css'
+import '../index.css';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 const NavBar = (props) => {
   if (props.signedIn){
     return (
-    <div className='navbar-in'>
+    <div className='bar-rectangle' id='navbar-in'>
       <div>
-              <img className='logo' src="https://tinyurl.com/mvc9bsk4" alt="ChipIn Logo"/>
-              <Link to="/">| Chip In</Link>
-            </div>
+        <Link to="/"><img className='logo'  src="https://tinyurl.com/mvc9bsk4" alt="ChipIn Logo" /></Link>
+      </div>
       <div className='links'>
-          <Link to="/community">Community</Link>
-          <Link to="/events">Find an Event</Link>
-          <Link to="/profile">You</Link>
-          <Link to="/signout" onClick={props.handleSignOut}>Log out</Link>
+        <Link to="/community" className='nav-item'>community</Link>
+        <Link to="/events" className='nav-item'>find an event</Link>
+        <Link to="/profile" className='nav-item'>you</Link>
+        <Link to="/signout" className='nav-item' id='sign-button' onClick={props.handleSignOut}>log out</Link>
       </div>
     </div>
     );
     }else{
       return (
-        <div className='navbar-out'>
+        <div className='bar-rectangle' id='navbar-out'>
             <div>
-              <img className='logo'  src="https://tinyurl.com/mvc9bsk4" alt="ChipIn Logo" />
-              <Link to="/">| Chip In</Link>
+              <Link to="/"><img className='logo' src="https://tinyurl.com/mvc9bsk4" alt="ChipIn Logo" /></Link>
             </div>
-            <div>
-              <Link to="/community">Community</Link>
-              <Link to="/signup" onClick={props.handleSignIn}>Sign In</Link>
+            <div className='links'>
+              <Link to="/community" className='nav-item'>community</Link>
+              <Link to="/signup" id='sign-button' className='nav-item' onClick={props.handleSignIn}>sign in</Link>
               {/* <button onClick={signInWithGoogle}> Sign in with Google </button> */}
             </div>
         </div>
